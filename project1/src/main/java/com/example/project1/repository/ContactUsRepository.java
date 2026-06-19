37e06d6f47e519599c972834da3e1421144dc9f1
musinichayadevi-lab <musinichayadevi@gmail.com>
2026-06-19 09:42:06 +0530

    Add ContactUsRepository interface for ContactUsEntity

---
+++ project1/src/main/java/com/example/project1/repository/ContactUsRepository.java
@@ -0,0 +1,13 @@
+package com.example.project1.repository;
+
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.example.project1.Entity.ContactUsEntity;
+
+@Repository
+public interface ContactUsRepository
+        extends JpaRepository<ContactUsEntity, Integer> {
+
+}
